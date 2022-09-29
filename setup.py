5e2531b15c56e38328e640b28a48318639022737
moshebeutel <moshebeutel@gmail.com>
2022-09-29 10:08:27 +0300

    version 0.0.3a0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="fed_prv_emg",
-    version="0.0.3",
+    version="0.0.3a0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
